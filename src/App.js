c355b69b6293646f52fab1c8b1745d0d82f505f5
tvai <tvai.gr@gmail.com>
2019-08-09 14:08:06 +0800

    video #70

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header';
 import HomePage from './pages/homepage';
 import ShopPage from './pages/shop';
 
@@ -10,6 +11,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
