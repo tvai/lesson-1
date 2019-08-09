e2875913490b5591d9775d535f2397966ee03d6a
tvai <tvai.gr@gmail.com>
2019-08-09 13:59:40 +0800

    video #69

---
+++ src/components/collection-item/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './index.styles.scss';
+
+const CollectionItem = (props) => {
+  const { name, price, imageUrl } = props;
+
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CollectionItem;
